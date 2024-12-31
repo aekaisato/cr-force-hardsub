16bc6183c459eb48276b19c20fdfab58e0208aa3
Aiden Sato <aiden@aekai.dev>
2024-12-31 01:47:37 -0800

    add permission check and warning

--- src/popup/index.js
+++ src/popup/index.js
@@ -33,7 +33,46 @@ const refreshSwitchHandler = async (ev) => {
   updateState();
 };
 
+const requiredPermissions = { origins: ["https://www.crunchyroll.com/*"] }
+
+const requestPermissions = () => {
+  browser.permissions.request(requiredPermissions, (success) => {
+    if (success) {
+      console.log("removing perms prompt.")
+      const a = document.querySelector("#perm-fail")
+      const parent = document.querySelector(".container")
+      parent.removeChild(a);
+    }
+  })
+}
+
+const checkPermissions = async () => {
+  console.log("checking permissions")
+  console.log((await browser.permissions.getAll()).origins)
+  browser.permissions.contains(requiredPermissions, (success) => {
+    if (!success) {
+      console.log("creating perms prompt.")
+      const check = document.querySelectorAll("#perm-fail")
+      if (check.length > 0) {
+        return
+      }
+      const a = document.createElement("a");
+      a.id = "perm-fail"
+      a.href = "javascript:void()";
+      a.onclick = requestPermissions
+      const text = document.createTextNode("insufficient permissions. click here to grant.")
+      a.appendChild(text);
+      const div = document.querySelector(".title");
+      const parent = document.querySelector(".container")
+      parent.insertBefore(a, div);
+    } else {
+      console.log("has permissions.")
+    }
+  })
+}
+
 hardsubSwitch.addEventListener("change", hardsubSwitchHandler);
 refreshSwitch.addEventListener("change", refreshSwitchHandler);
 
 updateState();
+checkPermissions();
